1f37472ae580e79b2b6eae210fdcbbad7b83db08
theoschneider <115454058+theoschneider@users.noreply.github.com>
2022-10-17 17:03:54 +0200

    Add files via upload

--- CNV-region-probes.R
+++ CNV-region-probes.R
@@ -4,34 +4,32 @@ folder <- list.files(path = "GWAS")
 studies <- read.csv("studies.tsv", sep = "\t", header = T)
 
 
-### Fichier différent pour chaque trait
-
-for(i in 1:length(folder)){
-    file <- read.csv(paste("GWAS/", folder[i], sep = ""), sep = "\t", header = T)
-    CNVregion <- file[file$chromosome == "16" & file$base_pair_location >= 29400000 & file$base_pair_location <= 30400000,]
-    write.table(CNVregion, paste("GWAS-CNV/", folder[i], sep = ""), sep = "\t", row.names = TRUE, col.names = TRUE)
-}
-
-
-
 ### Tout dans un même fichier
+
+# Isolate the right region, then find the smallest p-value in this region, then add the trait
 
 AllTraits <- data.frame()
 
 for(i in 1:length(folder)){
     file <- read.csv(paste("GWAS/", folder[i], sep = ""), sep = "\t", header = T)
-    CNVregion <- file[file$chromosome == "16" & file$base_pair_location >= 29400000 & file$base_pair_location <= 30400000,]
+    CNVregion <- file[file$chromosome == "16" 
+                      & file$base_pair_location >= 29400000 
+                      & file$base_pair_location <= 30400000,]
+    CNVregion <- CNVregion[CNVregion$p_value == min(CNVregion$p_value),]
     CNVregion$trait = studies$MAPPED_TRAIT[studies$STUDY.ACCESSION == sub("_.*", "", folder[i])]
     AllTraits <- rbind(AllTraits, CNVregion)
 }
 
 write.table(AllTraits, "GWAS-CNV/AllTraits.tsv", sep = "\t", row.names = F, col.names = T)
 
+length(unique(AllTraits$trait)) # check that we have 26 traits
 
 
-file <- read.csv(paste("GWAS/", folder[1], sep = ""), sep = "\t", header = T)
-CNVregion <- file[file$chromosome == "16" & file$base_pair_location >= 29400000 & file$base_pair_location <= 30400000,]
-CNVregion$trait = studies$MAPPED_TRAIT[studies$STUDY.ACCESSION == sub("_.*", "", folder[1])]
+
+
+
+
+
 
 
 
